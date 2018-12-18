b71479564d0b0a083029609e02e50eb4f138bf7b
Jim Madge <jmmadge@gmail.com>
2018-12-18 14:47:31 +0000

    Add test for cultural shift in communities

--- test/test_community.py
+++ test/test_community.py
@@ -1,5 +1,6 @@
 from . import context
 from guard import community
+from guard import parameters
 
 # Test the community class
 class TestCommunity(object):
@@ -12,3 +13,23 @@ class TestCommunity(object):
     def test_total_military_techs(self):
         self.community.military_techs = [False]*3 + [True]*7
         assert self.community.total_military_techs() == 7
+
+# Test cultural shift
+class TestCulturalShift(object):
+    # Set mutation to always occur
+    parameters.MUTATION_TO_ULTRASOCIETAL = 1
+    parameters.MUTATION_FROM_ULTRASOCIETAL = 1
+
+    def test_shift_to_true(self):
+        tile = community.Community()
+
+        tile.cultural_shift()
+        assert tile.total_ultrasocietal_traits() == parameters.N_ULTRASOCIETAL_TRAITS
+
+    def test_shift_to_false(self):
+        tile = community.Community()
+        tile.ultrasocietal_traits = [True]*parameters.N_ULTRASOCIETAL_TRAITS
+
+        tile.cultural_shift()
+        assert tile.total_ultrasocietal_traits() == 0
+
