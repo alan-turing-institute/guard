6a1324e96f2963d51c44bdf59120c2e708717242
Jim Madge <jmmadge@gmail.com>
2018-12-19 10:48:51 +0000

    Add conflict module

---
+++ guard/conflict.py
@@ -0,0 +1,45 @@
+from . import community, parameters
+from numpy.random import random
+
+# Determine the probability of a successful attack
+def success_probability(attacker, defender):
+    power_attacker = attacker.attack_power()
+    power_defender = defender.defence_power()
+
+    success = (power_attacker - power_defender) / (power_attacker + power_defender)
+    # Ensure probability is in the range [0,1]
+    if success < 0:
+        success = 0
+
+    return success
+
+# Determine the probability of ethnocide
+def ethnocide_probability(attacker, defender):
+    probability = parameters.ETHNOCIDE_MIN
+    probability += (parameters.ETHNOCIDE_MAX - parameters.ETHNOCIDE_MIN) * \
+            attacker.total_military_techs() / parameters.N_MILITARY_TECHS
+    probability -= parameters.ETHNOCIDE_ELEVATION_COEFFICIENT * defender.elevation
+
+    #Ensure probability is in the range [0,1]
+    if probability < 0:
+        probability = 0
+    elif probability > 1:
+        probability = 1
+
+    return probability
+
+# Conduct an attack
+def attack(attacker, defender, probability=None):
+    if probability == None:
+        probability = success_probability(attacker, defender)
+    # Determine whether attack was successful
+    if probability > random():
+        # Transfer defending community to attacker's polity
+        attacker.polity.transfer_community(defender)
+
+        # Attempt ethnocide
+        if ethnocide_probability(attacker, defender):
+            defender.ultrasocietal_traits = attacker.ultrasocietal_traits
+
+
+
---
+++ test/test_conflict.py
@@ -0,0 +1,71 @@
+from . import context
+from guard import community, conflict, parameters, polity
+import pytest
+
+# Create a strong polity, large with all ultrasocietal traits
+@pytest.fixture
+def strong_polity():
+    state_size = 20
+    state = polity.Polity([community.Community() for i in range(state_size)])
+    for tile in state.communities:
+        tile.ultrasocietal_traits = [True]*parameters.N_ULTRASOCIETAL_TRAITS
+    return state
+
+# Create a Mediocre polity
+@pytest.fixture
+def mediocre_polity():
+    state_size = 5
+    state = polity.Polity([community.Community() for i in range(state_size)])
+    return state
+
+# Create a weak polity, small with no ultrasocietal traits
+@pytest.fixture
+def weak_polity():
+    state_size = 1
+    state = polity.Polity([community.Community() for i in range(state_size)])
+    return state
+
+# Test success probability calculation
+class TestSuccessProbability(object):
+    # Ensure minimum of attack success probability is 0
+    def test_impossible_attack(self, strong_polity, weak_polity):
+        attacker = weak_polity.communities[0]
+        defender = strong_polity.communities[0]
+
+        probability = conflict.success_probability(attacker, defender)
+
+        assert probability == 0
+
+    # Calculate the probability of an almost certain victory
+    def test_almost_certain_attack(self, strong_polity, weak_polity):
+        attacker = strong_polity.communities[0]
+        defender = weak_polity.communities[0]
+
+        probability = conflict.success_probability(attacker,defender)
+
+        assert probability == 0.9900990099009901
+
+# Test attack routine
+class TestAttack(object):
+    # Test transfer of community after an attack which is certain to succeed
+    def test_certain_attack(self, strong_polity, mediocre_polity):
+        attacker = strong_polity
+        defender = mediocre_polity
+
+        attacking_community = attacker.communities[4]
+        defending_community = defender.communities[1]
+
+        conflict.attack(attacker=attacking_community, defender=defending_community, probability=1)
+
+        assert all([attacker.size() == 21, defender.size() == 4, \
+                defending_community in attacker.communities, \
+                defending_community not in defender.communities])
+
+# Test ethnocide routine
+class TestEthnocide(object):
+    # Ensure the ethnocide chance with no military tech or elevation is the base
+    def test_base_ethnocide(self, strong_polity, weak_polity):
+        attacking_community = strong_polity.communities[0]
+        defending_community = weak_polity.communities[0]
+
+        assert conflict.ethnocide_probability(attacking_community, defending_community) == parameters.ETHNOCIDE_MIN
