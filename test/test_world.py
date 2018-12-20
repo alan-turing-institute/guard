64db628004af4225d8d429c8df122786f27e0976
Jim Madge <jmmadge@gmail.com>
2018-12-20 16:05:31 +0000

    Update world tests

--- test/test_world.py
+++ test/test_world.py
@@ -3,24 +3,18 @@ from guard import world
 import pytest
 
 @pytest.fixture
-def simple_2x2_world():
-    map_ = world.World(xdim=2,ydim=2)
-    map_.create_flat_agricultural_world()
-
-    return map_
-
-@pytest.fixture
-def simple_3x3_world():
-    map_ = world.World(xdim=3,ydim=3)
-    map_.create_flat_agricultural_world()
-
-    return map_
+def generate_world():
+    def _generate_world(xdim,ydim):
+        map_ = world.World(xdim,ydim)
+        map_.create_flat_agricultural_world()
+        return map_
+    return _generate_world
 
 # Test the assignment of neighbours
 class TestNeighbours(object):
     # Ensure neighbours are correct in a 2x2 world
-    def test_neighbours_2x2(self,simple_2x2_world):
-        map_ = simple_2x2_world
+    def test_neighbours_2x2(self,generate_world):
+        map_ = generate_world(xdim=2,ydim=2)
 
         # Define correct neighbours
         neighbours_00 = {'left':None, 'right':map_.index(1,0), 'up':map_.index(0,1), 'down':None}
@@ -34,11 +28,16 @@ class TestNeighbours(object):
         assert map_.index(1,1).neighbours == neighbours_11
 
     # Ensure neighbours are correct in a 3x3 world
-    def test_neighbours_3x3(self,simple_3x3_world):
-        map_ = simple_3x3_world
+    def test_neighbours_3x3(self,generate_world):
+        map_ = generate_world(xdim=3,ydim=3)
 
         # Define correct neighbours
         neighbours_11 = {'left':map_.index(0,1), 'right':map_.index(2,1), \
                 'up':map_.index(1,2), 'down':map_.index(1,0)}
 
         assert map_.index(1,1).neighbours == neighbours_11
+
+def test_step(generate_world):
+    map_ = generate_world(xdim=5,ydim=5)
+    map_.step()
+
