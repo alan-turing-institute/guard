86dcc821980004cf25ab369957a759f11749f604
Jim Madge <jmmadge@gmail.com>
2019-04-04 11:02:01 +0100

    Add terrain docstrings

--- guard/terrain.py
+++ guard/terrain.py
@@ -1,4 +1,22 @@
+"""
+Terrain class and terrain types defintion
+"""
+
+
 class Terrain(object):
+    """
+    Terrain class.
+
+    Args:
+        name (str): A label describing the terrain type.
+        polity_formaing (bool): True if communities with this terrain may form
+            multi-community polities, False otherwise.
+
+    Attributes:
+        name (str): A label describing the terrain type.
+        polity_formaing (bool): True if communities with this terrain may form
+            multi-community polities, False otherwise.
+    """
     def __init__(self, name, polity_forming):
         self.name = name
         assert isinstance(polity_forming, bool)
@@ -8,7 +26,19 @@ class Terrain(object):
         return self.name
 
 
+"""
+Agricultural terrain
+"""
 agriculture = Terrain('agriculture', True)
+"""
+Steppe terrain, which begin the simulation with all military technologies
+"""
 steppe = Terrain('steppe', True)
+"""
+Sea terrain
+"""
 sea = Terrain('sea', False)
+"""
+Desert terrain
+"""
 desert = Terrain('desert', False)
